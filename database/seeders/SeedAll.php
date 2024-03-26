7ba7d1eba232954f77cc87f45b09b41273d4765f
Malik Samansya <maliksamansya206@gmail.com>
2024-03-26 12:22:54 +0700

    feat: update seeding data

--- database/seeders/SeedAll.php
+++ database/seeders/SeedAll.php
@@ -63,76 +63,108 @@ class SeedAll extends Seeder
                 'updated_at' => now(),
             ]);
         }
-        for ($i = 1; $i <= 9; $i++) {
-            DB::table('news')->insert([
-                'title' => "News Title $i",
-                'slug' => "news-title-$i",
-                'details' => "Details of news $i. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-                'image' => "avatar5.png",
-                'category_id' => 1, // Assuming category IDs range from 1 to 5
-                'status' => 1,
-                'featured' => rand(0, 1),
-                'view_count' => rand(100, 1000),
-                'created_at' => now(),
-                'updated_at' => now(),
-            ]);
+        // todo news
+        $json = file_get_contents(database_path('seed_data/all_data.json'));
+        $news = json_decode($json, true);
+        // news Add default values or modify the data as needed
+        foreach ($news as &$item) {
+            $item['featured'] = rand(0, 1);
+            $item['view_count'] = rand(100, 1000);
+            $item['created_at'] = now();
+            $item['updated_at'] = now();
         }
-        for ($i = 1; $i <= 3; $i++) {
-            DB::table('news')->insert([
-                'title' => "News Title $i",
-                'slug' => "news-title-$i",
-                'details' => "Details of news $i. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-                'image' => "avatar5.png",
-                'category_id' => 2, // Assuming category IDs range from 1 to 5
-                'status' => 1,
-                'featured' => rand(0, 1),
-                'view_count' => rand(100, 1000),
-                'created_at' => now(),
-                'updated_at' => now(),
-            ]);
-        }
-        for ($i = 1; $i <= 5; $i++) {
-            DB::table('news')->insert([
-                'title' => "News Title $i",
-                'slug' => "news-title-$i",
-                'details' => "Details of news $i. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-                'image' => "avatar5.png",
-                'category_id' => 3, // Assuming category IDs range from 1 to 5
-                'status' => 1,
-                'featured' => rand(0, 1),
-                'view_count' => rand(100, 1000),
-                'created_at' => now(),
-                'updated_at' => now(),
-            ]);
-        }
-        for ($i = 1; $i <= 5; $i++) {
-            DB::table('news')->insert([
-                'title' => "News Title $i",
-                'slug' => "news-title-$i",
-                'details' => "Details of news $i. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-                'image' => "avatar5.png",
-                'category_id' => 4, // Assuming category IDs range from 1 to 5
-                'status' => 1,
-                'featured' => rand(0, 1),
-                'view_count' => rand(100, 1000),
-                'created_at' => now(),
-                'updated_at' => now(),
-            ]);
-        }
+        DB::table('news')->insert($news);
 
-        for ($i = 1; $i <= 5; $i++) {
-            DB::table('news')->insert([
-                'title' => "News Title $i",
-                'slug' => "news-title-$i",
-                'details' => "Details of news $i. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-                'image' => "avatar5.png",
-                'category_id' => 5, // Assuming category IDs range from 1 to 5
-                'status' => 1,
-                'featured' => rand(0, 1),
-                'view_count' => rand(100, 1000),
-                'created_at' => now(),
-                'updated_at' => now(),
-            ]);
+        // todo settings
+        $json = file_get_contents(database_path('seed_data/settings.json'));
+        $settings = json_decode($json, true);
+        // settings Add default values or modify the data as needed
+        foreach ($settings as &$item) {
+            $item['created_at'] = now();
+            $item['updated_at'] = now();
         }
+        DB::table('settings')->insert($settings);
+
+        // todo advertisements
+        $json = file_get_contents(database_path('seed_data/advertisements.json'));
+        $advertisements = json_decode($json, true);
+        // settings Add default values or modify the data as needed
+        foreach ($advertisements as &$item) {
+            $item['created_at'] = now();
+            $item['updated_at'] = now();
+        }
+        DB::table('advertisements')->insert($advertisements);
+
+        // for ($i = 1; $i <= 9; $i++) {
+        //     DB::table('news')->insert([
+        //         'title' => "News Title $i",
+        //         'slug' => "news-title-$i",
+        //         'details' => "Details of news $i. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        //         'image' => "avatar5.png",
+        //         'category_id' => 1, // Assuming category IDs range from 1 to 5
+        //         'status' => 1,
+        //         'featured' => rand(0, 1),
+        //         'view_count' => rand(100, 1000),
+        //         'created_at' => now(),
+        //         'updated_at' => now(),
+        //     ]);
+        // }
+        // for ($i = 1; $i <= 3; $i++) {
+        //     DB::table('news')->insert([
+        //         'title' => "News Title $i",
+        //         'slug' => "news-title-$i",
+        //         'details' => "Details of news $i. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        //         'image' => "avatar5.png",
+        //         'category_id' => 2, // Assuming category IDs range from 1 to 5
+        //         'status' => 1,
+        //         'featured' => rand(0, 1),
+        //         'view_count' => rand(100, 1000),
+        //         'created_at' => now(),
+        //         'updated_at' => now(),
+        //     ]);
+        // }
+        // for ($i = 1; $i <= 5; $i++) {
+        //     DB::table('news')->insert([
+        //         'title' => "News Title $i",
+        //         'slug' => "news-title-$i",
+        //         'details' => "Details of news $i. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        //         'image' => "avatar5.png",
+        //         'category_id' => 3, // Assuming category IDs range from 1 to 5
+        //         'status' => 1,
+        //         'featured' => rand(0, 1),
+        //         'view_count' => rand(100, 1000),
+        //         'created_at' => now(),
+        //         'updated_at' => now(),
+        //     ]);
+        // }
+        // for ($i = 1; $i <= 5; $i++) {
+        //     DB::table('news')->insert([
+        //         'title' => "News Title $i",
+        //         'slug' => "news-title-$i",
+        //         'details' => "Details of news $i. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        //         'image' => "avatar5.png",
+        //         'category_id' => 4, // Assuming category IDs range from 1 to 5
+        //         'status' => 1,
+        //         'featured' => rand(0, 1),
+        //         'view_count' => rand(100, 1000),
+        //         'created_at' => now(),
+        //         'updated_at' => now(),
+        //     ]);
+        // }
+
+        // for ($i = 1; $i <= 5; $i++) {
+        //     DB::table('news')->insert([
+        //         'title' => "News Title $i",
+        //         'slug' => "news-title-$i",
+        //         'details' => "Details of news $i. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        //         'image' => "avatar5.png",
+        //         'category_id' => 5, // Assuming category IDs range from 1 to 5
+        //         'status' => 1,
+        //         'featured' => rand(0, 1),
+        //         'view_count' => rand(100, 1000),
+        //         'created_at' => now(),
+        //         'updated_at' => now(),
+        //     ]);
+        // }
     }
 }
